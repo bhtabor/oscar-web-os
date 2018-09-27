b8daa35b76ef2edc190c43f4f2e343c02319a78c
Pirun Seng <sengpirun.rain@gmail.com>
2018-09-27 11:30:56 +0700

    Remove comments in Bullet config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,10 +60,5 @@ Rails.application.configure do
     Bullet.enable = true
     Bullet.alert = true
     Bullet.bullet_logger = true
-    # Bullet.console = true
-    # Bullet.rails_logger = true
-    # Bullet.add_footer = true
-    # Bullet.stacktrace_includes = true
-    # Bullet.raise = true
   end
 end
